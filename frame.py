16cb07de7fa2d61390bd2998abfb9c1d1180bafc
Shuvashish Chatterjee <shuvashish.chatterjee@clytics.com>
2019-05-17 13:34:01 +0530

    fix: corrected frame

--- frame.py
+++ frame.py
@@ -25,6 +25,58 @@ classes = [#
     'sink','refrigerator','book','clock','vase','scissors',\
     'teddy bear','hair drier','toothbrush' ]
 
+
+
+class OBSTACLE:
+  def __init__(self , bbox, score, category,_id, image) :
+    self.bbox = bbox
+    self.score =  score
+    self.category =  category
+    self._id = _id
+    self.image = image
+    self.first_detect =  False
+    self.position = None
+    self.history : np.ndarray = None
+    self.vx = 0
+    self.vy= 0
+    self.gridx : int
+    self.gridy : int
+
+class VEHICLE(OBSTACLE) :
+  def __init__(self) :
+    self.numplate=None
+    self.rx = None
+    self.ry = None
+    self.vx = 0
+    self.first = True
+    
+  def detect_number_plate(self):
+    return None
+  
+  def detect_position(self) :
+    return None
+  
+  def detect_velocity(self) :
+    return None
+    
+  
+    
+    
+class TRAFFIC_LIGHTS(OBSTACLE) :
+  def __init__(self) :
+    return None
+  
+  def detect_status(self):
+    return None
+    
+class TRAFFIC_SIGNS(OBSTACLE):
+  def __init__(self) :
+    return None
+  
+  def decipher(self):
+    return None
+
+
 class FRAME :
 
 
@@ -37,7 +89,8 @@ class FRAME :
         "image":None,
         "UNWARPED_SIZE" : [ 500, 600],
         "WRAPPED_WIDTH" :  530,
-        "LANE_WIDTH" :  3.5
+        "LANE_WIDTH" :  3.5,
+        "fps" :22
         }
 
     @classmethod
@@ -48,6 +101,7 @@ class FRAME :
             return "Unrecognized attribute name '" + n + "'"  
 
     def __init__(self, **kwargs):
+        self.fps:float
         self.UNWARPED_SIZE :[int,int]
         self.LANE_WIDTH :int
         self.WRAPPED_WIDTH  :  int
@@ -67,6 +121,7 @@ class FRAME :
         self.yolo =  YOLO(score =  0.3, iou =  0.5, gpu_num = 0)
         self.first_detect = True
         self.trackers = []
+        self.obstacles :[OBSTACLE] =[]
         
 
     def perspective_tfm(self ,  image) : 
@@ -142,6 +197,15 @@ class FRAME :
         out_boxes =  out_boxes[obst_idx,:]
         out_scores =  out_scores[obst_idx]
         out_classes = out_classes[obst_idx]
+        for i in range(out_classes.shape):
+            tracker = cv2.TrackerKCF_create()  # Note: Try comparing KCF with MIL
+            bbox =  out_boxes[i,:]
+            
+            corwh = self.bbox2corwh(out_boxes[i,:])
+            success = tracker.init(image, tuple(corwh))
+            if success :
+                self.trackers.append(tracker)
+                
         if not self.first_detect :
             self.update_trackers(image)
         return
@@ -149,39 +213,54 @@ class FRAME :
     def calculate_position(self, bbox):
         if (self.perspective_done_at > 0):
             pos = np.array((bbox[0]/2+bbox[2]/2, bbox[3])).reshape(1, 1, -1)
-            dst = cv2.perspectiveTransform(pos, self.M).reshape(-1, 1)
-            return np.array((self.UNWARPED_SIZE[1]-dst[1])/self.pix_per_meter_y)
+            dst = self.perspective_tfm(pos).reshape(-1, 1)
+            # dst = cv2.perspectiveTransform(pos, self.M).reshape(-1, 1)
+            return np.array((dst[0]/self.pix_per_meter_x,self.UNWARPED_SIZE[1]-dst[1])/self.pix_per_meter_y)
         else:
-            return np.array([0])
+            return np.array([0,0])
+    
+    @staticmethod
+    def bbox2corwh(bbox):
+        xmin = int(bbox[0])
+        ymin = int(bbox[1])
+        xmax = int(bbox[2]) 
+        ymax = int(bbox[3] )
+        return np.array([xmin, ymin,xmax-xmin,ymax-ymin])
 
+    @staticmethod
+    def corwh2bbox(corwh):
+        xmin = int(corwh[0])
+        ymin = int(corwh[1])
+        xmax = int(corwh[0] + corwh[2])
+        ymax = int(corwh[1] + corwh[3])
+        return np.array([xmin, ymin,xmax,ymax])
+
+    
     def update_trackers(self, image):
         boxes = []
-        color = (80, 220, 60)
-        fontface = cv2.FONT_HERSHEY_SIMPLEX
-        fontscale = 1
-        thickness = 1
+        not_present = []
 
         for n, pair in enumerate(self.trackers):
             tracker, car = pair
-            textsize, _baseline = cv2.getTextSize(
-                car, fontface, fontscale, thickness)
-            success, bbox = tracker.update(image)
+            success, corwh = tracker.update(image)
 
             if not success:
+                not_present.append(n)
+                del self.obstacles[n]
                 del self.trackers[n]
                 continue
+            bbox =  self.corwh2bbox(corwh)
+            self.obstacles[n]= self.update_obstacle(self.obstacles[n], bbox)
 
-            boxes.append(bbox)  # Return updated box list
 
-            xmin = int(bbox[0])
-            ymin = int(bbox[1])
-            xmax = int(bbox[0] + bbox[2])
-            ymax = int(bbox[1] + bbox[3])
-            xmid = int(round((xmin+xmax)/2))
-            ymid = int(round((ymin+ymax)/2))
-            # determine velocity , rpx, rpy
+        return boxes
 
-        return boxes, counters
+    def update_obstacle(self, obstacle: OBSTACLE ,  bbox) :
+        obstacle.bbox = bbox
+        obstacle.history.append(obstacle.position)
+        obstacle.position = self.calculate_position(bbox)
+        obstacle.velocity = (obstacle.position - obstacle.history[0,:]) * self.fps
+        return obstacle
 
 
     def remove_tracked(self) :
@@ -198,48 +277,5 @@ class FRAME :
     def vehicle_speed(self) :
         return
 
-class OBSTACLES:
-  def __init__(self , bbox, score, category,_id, image) :
-    self.bbox = bbox
-    self.score =  score
-    self.category =  category
-    self._id = _id
-    self.image = image
-    self.first_detect =  False
-    self.gridx : int
-    self.gridy : int
-    
 
-class VEHICLE(OBSTACLES) :
-  def __init__(self) :
-    self.numplate=None
-    self.rx = None
-    self.ry = None
-    self.vx = 0
-    self.first = True
     
-  def detect_number_plate(self):
-    return None
-  
-  def detect_position(self) :
-    return None
-  
-  def detect_velocity(self) :
-    return None
-    
-  
-    
-    
-class TRAFFIC_LIGHTS(OBSTACLES) :
-  def __init__(self) :
-    return None
-  
-  def detect_status(self):
-    return None
-    
-class TRAFFIC_SIGNS(OBSTACLES):
-  def __init__(self) :
-    return None
-  
-  def decipher(self):
-    return None
