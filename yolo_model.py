2d68155096bcf80e4097b0178a9b6448b63270a9
Shuvashish Chatterjee <shuvashish.chatterjee@gmail.com>
2019-06-14 10:41:19 +0530

    printoptions

--- yolo_model.py
+++ yolo_model.py
@@ -7,7 +7,7 @@ from keras.models import Model
 import struct
 import cv2
 
-np.set_printoptions(threshold=np.nan)
+# np.set_printoptions(threshold=np.nan)
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
